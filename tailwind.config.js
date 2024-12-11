0dca3200c1179ba3b31611d89b435bfffba6ace0
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-12-11 08:51:17 +0600

    Basic navbar creation done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layout/**/*.liquid",
+    "./templates/**/*.liquid",
+    "./snippets/**/*.liquid",
+    "./sections/**/*.liquid",
+    "./assets/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
