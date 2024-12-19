5d214f4187a3e635dd756750ada6a8d8a530ff7f
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-12-19 12:18:23 +0600

    Article template and editing structure done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "./sections/**/*.liquid",
     "./assets/**/*.js",
   ],
+  safelist: ["bg-red-200", "h-full", "text-red-500", "w-full", "h-screen"],
   theme: {
     extend: {},
   },
